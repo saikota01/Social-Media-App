71367500f64d4e7e4152237f45a646c083556822
saikota01 <saikotareddy07@gmail.com>
2023-09-30 23:50:22 +0530

    final changes2

--- server/dbConnection.js
+++ server/dbConnection.js
@@ -1,5 +1,5 @@
 // getting-started.js
 const mongoose = require('mongoose');
-const DB_URL = 'mongodb+srv://admin:admin@cluster0.ysm9mum.mongodb.net/?retryWrites=true&w=majority'
+const DB_URL = 'mongodb+srv://admin:test@cluster0.ysm9mum.mongodb.net/?retryWrites=true&w=majority'
 
 mongoose.connect(DB_URL).then(()=>console.log('Connected to DB!')).catch(err=>console.log(err));
